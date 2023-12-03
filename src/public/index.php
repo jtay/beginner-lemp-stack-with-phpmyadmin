7ac7eaad6e0975ff577dbedca753c39568b9ce64
Jaydon Taylor <jaydon.taylor@onstate.co.uk>
2023-12-03 14:07:17 +0000

    "Add Docker LEMP stack configuration"
    Adds initial Docker Compose setup for LEMP stack environment
    Adds configuration for PHPMyAdmin, Nginx, PHP, and MySQL.
    Adds sample environment variables
    Adds Nginx default server configuration
    Adds placeholder for MySQL data directory
    Add webroot index.php

---
+++ src/public/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>🎉 It works!</title>
+    <style>
+        ul.horizontal-list {
+            list-style-type: none; 
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+        }
+
+        ul.horizontal-list li {
+            display: inline-block;
+        }
+
+        ul.horizontal-list li:not(:first-child):before {
+            content: '|'; 
+            color: #a0a0a0;
+            margin: 1.75rem;
+        }
+
+        p {
+            margin-bottom: 0.5rem;
+        }
+    </style>
+</head>
+<body>
+
+    <center>
+        <h1> 🎉 It works! </h1>
+        <p> Hello world <p>
+        <ul class="horizontal-list">
+            <li>
+                <a href="https://github.com/jtay/docker-lemp" target="_blank">Star on GitHub</a>
+            </li>
+            <li>
+                <a href="https://jaydon-dev.co.uk" target="_blank">Say hello!</a>
+            </li>
+        </ul>
+    </center>
+
+    <hr />
+
+    <?=
+        phpinfo();
+    ?>
+
+</body>
+</html>
